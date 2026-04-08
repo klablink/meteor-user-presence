d697db6ae16b2ddfe2364a365ec806d19fbb5c83
d.ruggeroni <d.ruggeroni@lectra.com>
2026-04-08 10:26:12 +0200

    fix(build): error building application

--- package.js
+++ package.js
@@ -6,7 +6,7 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-    api.versionsFrom(['2.13.3', '3.0',"3.3.2"]);
+    api.versionsFrom(['2.13.3', '3.0',"3.3.2","3.4"]);
 
     api.use('tracker');
     api.use('check');
